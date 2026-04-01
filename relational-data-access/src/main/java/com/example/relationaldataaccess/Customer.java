6188426b17d61c1905fb85f6e57686286ca0f293
nagaminejun <nagaminejun1988@gmail.com>
2026-04-01 16:56:04 +0900

    Rename folders and organize structure

--- relational-data-access/src/main/java/com/example/relationaldataaccess/Customer.java
+++
@@ -1,9 +0,0 @@
-package com.example.relationaldataaccess;
-
-public record Customer(long id, String firstName, String lastName) {
-  
-  @Override
-  public String toString() {
-    return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
-  }
-}
